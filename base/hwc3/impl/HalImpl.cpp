9122bd83d4392221b0b2962878c9aed96aee96cd
Long Ling <longling@google.com>
2022-03-01 09:30:21 -0800

    hwc3: return OK for getDisplayPhysicalOrientation
    Bug: 221885596
    Test: atest PerInstance/GraphicsComposerAidlTest#GetDisplayPhysicalOrientation/0_android_hardware_graphics_composer3_IComposer_default
    Change-Id: I91d4a42f92e9d41573a1ad2b75c7be28c170141c

--- base/hwc3/impl/HalImpl.cpp
+++ base/hwc3/impl/HalImpl.cpp
@@ -406,7 +406,7 @@ int32_t HalImpl::getDisplayedContentSamplingAttributes(
 int32_t HalImpl::getDisplayPhysicalOrientation([[maybe_unused]] int64_t display,
                                                [[maybe_unused]] common::Transform* orientation) {
     // TODO(b/214461751): return the physical orientation of the display
-    return HWC2_ERROR_UNSUPPORTED;
+    return HWC2_ERROR_NONE;
 }
 
 int32_t HalImpl::getDozeSupport(int64_t display, bool* support) {
